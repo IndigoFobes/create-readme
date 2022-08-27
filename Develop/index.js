e079cf8e75b34f8c2a820c0cc45b06d2fc2fba24
Indigo <indigo.crandell@gmail.com>
2022-08-27 16:46:12 -0400

    Basic set up. Upgraded and then downgraded my version of inquirer. Created a questions array.

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,16 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = ['What is the title of your project?', 'Please enter a description of your project:', 'Please explain how to install your application:', 'Please explain how to use your application:', 'Please explain contribution guideluines:', 'Please provide test instructions:'];
+console.log(questions);
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
+
+console.log(process.argv);
 
 // TODO: Create a function to initialize app
 function init() {}
